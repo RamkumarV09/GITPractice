2369321e8af144c4637a94fc1f7f8c9375294054
RamkumarV09 <ramkumarv0986@gmail.com>
2025-03-12 22:56:23 +0530

    Green Code

--- src/test/java/org/sample/SampleGIT.java
+++ src/test/java/org/sample/SampleGIT.java
@@ -1,19 +1,30 @@
 package org.sample;
 
 public class SampleGIT {
-	
+
 	public void meth1() {
-		
+
 		System.out.println("Meth 1");
 	}
-	
-    public void meth2() {
-		
+
+	public void meth2() {
+
 		System.out.println("Meth 2");
 	}
 
-    public void meth3() {
-		
+	public void meth3() {
+
 		System.out.println("Meth 3");
 	}
+
+	public void meth6() {
+
+		System.out.println("Meth 6");
+	}
+
+	public void meth7() {
+
+		System.out.println("Meth 7");
+	}
+
 }
